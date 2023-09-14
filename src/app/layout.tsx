734f918a62e0797d17dcc984f64b42898e6ec481
Raghav Rudhra <raghavrudhra28@gmail.com>
2023-09-14 16:20:46 -0400

    fixing nextjs logic for rootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,17 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import ThemeRegistry from "./themeRegistry";
 import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth";
-import NavBar from "./Navbar/page";
 export default function RootLayout(props: {
   session: Session;
   children: React.ReactNode;
 }) {
   const { session, children } = props;
   // const mode = localStorage.getItem("mode");
-  const [mode, setMode] = useState<string>();
-
-  useEffect(() => {
-    if (mode !== undefined) {
-      localStorage.setItem("mode", mode);
-    }
-  }, [mode]);
 
   return (
     <html lang="en">
       <body>
-        <ThemeRegistry options={{ key: "mui" }} mode={mode}>
-          <SessionProvider session={session}>
-            <NavBar setMode={setMode} mode={mode} />
-            {children}
-          </SessionProvider>
-        </ThemeRegistry>
+        <SessionProvider session={session}>{children}</SessionProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,24 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import Home from "./Home/page";
-export default async function Homepage() {
+import NavBar from "./Navbar/page";
+import ThemeRegistry from "./themeRegistry";
+export default function Homepage() {
+  const [mode, setMode] = useState<string>();
+  useEffect(() => {
+    setMode(localStorage.getItem("mode"));
+  }, []);
+  useEffect(() => {
+    if (mode !== undefined) {
+      localStorage.setItem("mode", mode);
+    }
+  }, [mode]);
   return (
     <main>
-      <Home />
+      <ThemeRegistry options={{ key: "mui" }} mode={mode}>
+        <NavBar setMode={setMode} mode={mode} />
+        <Home />
+      </ThemeRegistry>
     </main>
   );
 }
