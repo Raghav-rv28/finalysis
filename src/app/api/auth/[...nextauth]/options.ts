24487ef360295c32003d5c5dd2d7f038cacf62d8
Raghav Rudhra <raghavrudhra28@gmail.com>
2023-09-15 11:15:44 -0400

    adding google login

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,37 +1,44 @@
 import GitHubProvider from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials"
+import CredentialsProvider from "next-auth/providers/credentials";
 import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-
-const options:NextAuthOptions ={
-    providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-        }),
-        CredentialsProvider({
-            name: "Credentials",
-            credentials:{
-                userName:{
-                    label: "Name:",
-                    type: "text", 
-                    placeholder: "Enter Name",
-                },
-                password: {
-                    label: "password",
-                    type: "text",
-                    placeholder: "",
-                }
-            },
-            async authorize(credentials) {
-                //get data from database.
-                const user = {id:"0",userName: "Raj", password: "nextAuth"};
-                if(credentials?.userName === user.userName && credentials.password === user.password){
-                    return user
-                }
-                return null
-            }
-        }),
-    ],
+const options: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        userName: {
+          label: "Name:",
+          type: "text",
+          placeholder: "Enter Name",
+        },
+        password: {
+          label: "password",
+          type: "text",
+          placeholder: "",
+        },
+      },
+      async authorize(credentials) {
+        //get data from database.
+        const user = { id: "0", userName: "Raj", password: "nextAuth" };
+        if (
+          credentials?.userName === user.userName &&
+          credentials.password === user.password
+        ) {
+          return user;
+        }
+        return null;
+      },
+    }),
+  ],
 };
 export default options;
