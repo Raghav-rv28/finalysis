18c249d2ac91a74b1ed82cb61947be2780dad196
Raghav Rudhra <raghavrudhra28@gmail.com>
2023-09-23 01:20:40 -0400

    adding logs

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -3,9 +3,15 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-console.log(`Testing: ${process.env.NEXTAUTH_SECRET}`);
+console.log(
+  `Testing: ${process.env.NEXTAUTH_SECRET} ${
+    process.env.NEXT_PUBLIC_SECRET as string
+  }`
+);
 const options: NextAuthOptions = {
-  secret: process.env.NEXTAUTH_SECRET as string,
+  secret:
+    (process.env.NEXTAUTH_SECRET as string) ||
+    (process.env.NEXT_PUBLIC_SECRET as string),
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
