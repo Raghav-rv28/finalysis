060032a552d682e8e4b33f95338dc03669a621b6
Raghav Rudhra <raghavrudhra28@gmail.com>
2023-09-23 01:00:21 -0400

    adding type assertion

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -4,7 +4,7 @@ import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 const options: NextAuthOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET as string,
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID as string,
