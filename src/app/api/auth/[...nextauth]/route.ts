d71fb8b5badc444a26fd075d2317b500e9d6ba99
Raghav Rudhra <raghavrudhra28@gmail.com>
2023-11-22 13:37:56 -0500

    testing changes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth"
-import options from "./options"
-const handler = NextAuth(options)
+import NextAuth from "next-auth";
+import options from "./options";
+console.log(process.env.COGNITO_CLIENT_ID);
+const handler = NextAuth(options);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
